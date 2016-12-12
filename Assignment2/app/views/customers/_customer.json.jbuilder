7957c99a1ce1d040508e78422cece059463cb5e2
Manas <manas.k@reportgarden.com>
2016-12-12 18:40:01 +0530

    Removed all

--- Assignment2/app/views/customers/_customer.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! customer, :id, :name, :email, :city, :pincode, :created_at, :updated_at
-json.url customer_url(customer, format: :json)
--- Assignment2/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  resources :orders
-  resources :items
-  resources :customers
-
-  get '/customer_orders/:customer_id', to: 'orders#customer_orders', as: "customer_order"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
