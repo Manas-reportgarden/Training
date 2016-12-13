ce23db000fd5314543561b067d4b8fd0a92f1961
Manas <manas.k@reportgarden.com>
2016-12-13 11:38:01 +0530

    Added form validation for assignment 1 and changed routes for assignment 2

--- Assignment2/config/routes.rb
+++ Assignment2/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :items
   resources :customers
 
+  root 'orders#new'
   get '/customer_orders/:customer_id', to: 'orders#customer_orders', as: "customer_order"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
