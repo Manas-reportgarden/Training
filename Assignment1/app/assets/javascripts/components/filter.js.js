e73803358a80e851dfdfabe016605d1567d0b3cb
Manas <manas.k@reportgarden.com>
2016-12-28 12:03:09 +0530

    Working total

--- Assignment1/app/assets/javascripts/components/filter.js.js
+++ Assignment1/app/assets/javascripts/components/filter.js.js
@@ -1,28 +1,67 @@
 
   render: function() {
+
+    var rows = this.rowGenerator(),
+    total = this.totalGenerator();
     return (React.createElement("div", null, React.createElement("table", null, 
-       React.createElement("thead", null, 
+      React.createElement("thead", null, 
         Object.values(this.props.input_display_arr).map(function(header){
           return React.createElement("th", null, header)
         },this)
       
     ), 
     React.createElement("tbody", null, 
-      
-        Object.keys(this.props.students).map(function(value){
-          debugger
-          return this.props.students[value].map(function(display){
-            debugger
-            return (React.createElement("tr", null, Object.values(this.props.input_display_arr).map(function(subject){
-              debugger
-              return React.createElement("td", null, display[subject])
-            },this)))
-          }, this)
-        }, this)
-      
+      rows
     )
 
+  )
+));
+},
+rowGenerator: function(){
+  return Object.keys(this.props.students).map(function(value){
+
+    return ([React.createElement("tr", null, this.props.students[value].map(function(display){
+
+      return (React.createElement("tr", null, Object.values(this.props.input_display_arr).map(function(subject){
+        debugger
+        return React.createElement("td", null, display[subject])
+      },this)))
+    }, this)),
+    React.createElement("tr", null, 
+      React.createElement("td", null, "Total"), 
+      React.createElement(TotalRow, {students: this.props.students[value], 
+        input_display_arr: this.props.input_display_arr})
     )
-    ));
+  ])
+}, this)
+},
+totalGenerator: function(){
+
 }
+
+});
+
+var TotalRow = React.createClass({displayName: "TotalRow",
+  getInitialState: function(){
+    return { 'maths_total': 0}
+  },
+  addMaths: function(value){
+    this.props.students.map(function(subject){
+      this.state.maths_total = this.state.maths_total + subject[value]
+    }, this)
+    {debugger}
+    return this.state.maths_total;
+
+  },
+
+  render: function(){
+    return (React.createElement("td", null, 
+      Object.values(this.props.input_display_arr).map(function(subject_name){
+        return (subject_name == 'maths')? (
+          this.addMaths(subject_name)
+        ): ('');
+      }, this)
+        ));
+
+  }
 });
--- Assignment1/app/assets/javascripts/components/filter.js.jsx
+++ Assignment1/app/assets/javascripts/components/filter.js.jsx
@@ -1,29 +1,68 @@
 var Filter = React.createClass({
 
   render: function() {
+
+    var rows = this.rowGenerator(),
+    total = this.totalGenerator();
     return (<div><table>
-       <thead>
+      <thead>
         {Object.values(this.props.input_display_arr).map(function(header){
           return <th>{header}</th>
         },this)
       }
     </thead>
     <tbody>
-      {
-        Object.keys(this.props.students).map(function(value){
-          debugger
-          return this.props.students[value].map(function(display){
-            debugger
-            return (<tr>{Object.values(this.props.input_display_arr).map(function(subject){
-              debugger
-              return <td>{display[subject]}</td>
-            },this)}</tr>)
-          }, this)
-        }, this)
-      }
+      {rows}
     </tbody>
 
-    </table>
-    </div>);
+  </table>
+</div>);
+},
+rowGenerator: function(){
+  return Object.keys(this.props.students).map(function(value){
+
+    return ([<tr>{this.props.students[value].map(function(display){
+
+      return (<tr>{Object.values(this.props.input_display_arr).map(function(subject){
+        debugger
+        return <td>{display[subject]}</td>
+      },this)}</tr>)
+    }, this)}</tr>,
+    <tr>
+      <td>Total</td>
+      <TotalRow  students={this.props.students[value]}
+        input_display_arr={this.props.input_display_arr} />
+    </tr>
+  ])
+}, this)
+},
+totalGenerator: function(){
+
 }
+
+});
+
+var TotalRow = React.createClass({
+  getInitialState: function(){
+    return { 'maths_total': 0}
+  },
+  addMaths: function(value){
+    this.props.students.map(function(subject){
+      this.state.maths_total = this.state.maths_total + subject[value]
+    }, this)
+    {debugger}
+    return this.state.maths_total;
+
+  },
+
+  render: function(){
+    return (<td>{
+      Object.values(this.props.input_display_arr).map(function(subject_name){
+        return (subject_name == 'maths')? (
+          this.addMaths(subject_name)
+        ): ('');
+      }, this)
+        }</td>);
+
+  }
 });
