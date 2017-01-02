b29f509760de2fa52092502594ae942af707b117
Manas <manas.k@reportgarden.com>
2017-01-02 16:46:15 +0530

    working version 2.0

--- Assignment1/app/assets/javascripts/components/filter.js.jsx
+++ Assignment1/app/assets/javascripts/components/filter.js.jsx
@@ -1,150 +1,155 @@
 var Filter = React.createClass({
 
-  render: function() {
+    render: function() {
 
-    var rows = this.rowGenerator();
-    return (<div><table>
-      <thead><tr>
-        {Object.values(this.props.input_display_arr).map(function(header){
-          return <th>{header}</th>
-        },this)
-      }
-    </tr>
-    </thead>
-    <tbody>
-      {rows}
-    </tbody>
-
-  </table>
-</div>);
-},
-
-rowGenerator: function(){
-
-  return Object.keys(this.props.students).map(function(value){
-
-    return ([this.props.students[value].map(function(display){
-
-      return (<tr>{Object.values(this.props.input_display_arr).map(function(subject){
-        debugger
-        return <td>{display[subject]}</td>
-      },this)}</tr>)
-    }, this),
-      this.props.should_total == 'true' ?
-      (<TotalRow  students={this.props.students[value]}
-        input_display_arr={this.props.input_display_arr} />) : ''
-    ,
-      this.props.should_compare == 'true' &&  this.props.input_group_by == 'student_id' ?
-      (<CompareRow  students={this.props.students[value]}
-        input_display_arr={this.props.input_display_arr}
-        year_1={this.props.year_1}
-        year_2={this.props.year_2} />) : ''
-
-  ])
-}, this)
+        var rows = this.rowGenerator();
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            {Object.values(this.props.input_display_arr).map(function(header) {
+                                return <th>{header}</th>
+                            }, this)
 }
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
+
+                </table>
+            </div>
+        );
+    },
+
+    rowGenerator: function() {
+
+        return Object.keys(this.props.students).map(function(value) {
+
+            return ([
+                this.props.students[value].map(function(display) {
+
+                    return (
+                        <tr>{Object.values(this.props.input_display_arr).map(function(subject) {
+
+                                return <td>{display[subject]}</td>
+                            }, this)}</tr>
+                    )
+                }, this),
+                this.props.should_total == 'true'
+                    ? (<TotalRow students={this.props.students[value]} input_display_arr={this.props.input_display_arr}/>)
+                    : '',
+                this.props.should_compare == 'true' && this.props.input_group_by == 'student_id'
+                    ? (<CompareRow students={this.props.students[value]} input_display_arr={this.props.input_display_arr} year_1={this.props.year_1} year_2={this.props.year_2}/>)
+                    : ''
+
+            ])
+        }, this)
+    }
 
 });
 
 var CompareRow = React.createClass({
 
-  getInitialState: function(){
-    return { 'compare_maths': 0,
-      'compare_physics': 0,
-      'compare_chemistry': 0
+    getInitialState: function() {
+        return {'compare_maths': 0, 'compare_physics': 0, 'compare_chemistry': 0}
+    },
+    compare: function() {
+        var compare_year_1 = '',
+            compare_year_2 = ''
+        this.props.students.map(function(student) {
+            if (student['year'] == parseInt(this.props.year_1) && (student))
+                compare_year_1 = student
+            if (student['year'] == parseInt(this.props.year_2) && (student))
+                compare_year_2 = student
+        }, this)
+
+        return Object.values(this.props.input_display_arr).map(function(subject_name) {
+            var compare_marks = 0
+            if (compare_year_1)
+                compare_marks = Math.abs(compare_marks - compare_year_1[subject_name])
+            if (compare_year_2)
+                compare_marks = Math.abs(compare_marks - compare_year_2[subject_name])
+
+            if (['maths', 'physics', 'chemistry'].includes(subject_name)) {
+                return (
+                    <td>{compare_marks}</td>
+                )
+            }
+
+        }, this)
+
+    },
+
+    render: function() {
+        var compareRow = this.compare();
+        return (
+            <tr>
+                <td>Change</td>
+                {compareRow}
+            </tr>
+        );
     }
-  },
-  compare: function(){
-    var compare_year_1 = '', compare_year_2 = ''
-    this.props.students.map(function(student){
-      if (student['year']  == parseInt(this.props.year_1) && (student))
-      compare_year_1 = student
-      if (student['year']  == parseInt(this.props.year_2) && (student))
-      compare_year_2 = student
-    }, this)
-
-
-    return Object.values(this.props.input_display_arr).map(function(subject_name){
-      var compare_marks = 0
-      if(compare_year_1)
-      compare_marks = Math.abs(compare_marks - compare_year_1[subject_name])
-      if(compare_year_2)
-      compare_marks = Math.abs(compare_marks - compare_year_2[subject_name])
-
-      if(['maths', 'physics', 'chemistry'].includes(subject_name)){
-        return (<td>{compare_marks}</td>)
-      }
-
-    }, this)
-
-  },
-
-  render: function(){
-    var compareRow = this.compare();
-    return (<tr>
-      <td>Change</td>
-      {compareRow}
-    </tr>);
-  }
 });
 
 var TotalRow = React.createClass({
-  getInitialState: function(){
-    return { 'maths_total': 0,
-      'physics_total': 0,
-      'chemistry_total': 0}
+    getInitialState: function() {
+        return {'maths_total': 0, 'physics_total': 0, 'chemistry_total': 0}
     },
-    add: function(subject_name){
-      if (subject_name == 'maths'){
-        return this.addMaths(subject_name);
-      }
-      else if (subject_name == 'physics'){
-        return this.addPhysics(subject_name);
-      }
-      else if (subject_name == 'chemistry'){
-        return this.addChemistry(subject_name);
-      }
-      else {
-        return " "
-      }
+    add: function(subject_name) {
+        if (subject_name == 'maths') {
+            return this.addMaths(subject_name);
+        } else if (subject_name == 'physics') {
+            return this.addPhysics(subject_name);
+        } else if (subject_name == 'chemistry') {
+            return this.addChemistry(subject_name);
+        } else {
+            return " "
+        }
 
     },
-    addMaths: function(value){
-      this.props.students.map(function(subject){
-        this.state.maths_total = this.state.maths_total + subject[value]
-      }, this)
-      {debugger}
-      return (<td>{this.state.maths_total}</td>);
+    addMaths: function(value) {
+        this.props.students.map(function(subject) {
+            this.state.maths_total = this.state.maths_total + subject[value]
+        }, this)
+        return (
+            <td>{this.state.maths_total}</td>
+        );
 
     },
-    addPhysics: function(value){
-      this.props.students.map(function(subject){
-        this.state.physics_total = this.state.physics_total + subject[value]
-      }, this)
-      {debugger}
-      return (<td>{this.state.physics_total}</td>);
+    addPhysics: function(value) {
+        this.props.students.map(function(subject) {
+            this.state.physics_total = this.state.physics_total + subject[value]
+        }, this)
+        return (
+            <td>{this.state.physics_total}</td>
+        );
 
     },
-    addChemistry: function(value){
-      this.props.students.map(function(subject){
-        this.state.chemistry_total = this.state.chemistry_total + subject[value]
-      }, this)
-      {debugger}
-      return (<td>{this.state.chemistry_total}</td>);
+    addChemistry: function(value) {
+        this.props.students.map(function(subject) {
+            this.state.chemistry_total = this.state.chemistry_total + subject[value]
+        }, this)
+        return (
+            <td>{this.state.chemistry_total}</td>
+        );
 
     },
     totalGenerator: function() {
-      return Object.values(this.props.input_display_arr).map(function(subject_name){
-          return this.add(subject_name);
+        return Object.values(this.props.input_display_arr).map(function(subject_name) {
+            return this.add(subject_name);
 
         }, this)
     },
-    render: function(){
-      var total = this.totalGenerator();
-      return (<tr>
-        <td>Total</td>
-        {total}
-      </tr>)
+    render: function() {
+        var total = this.totalGenerator();
+        return (
+            <tr>
+                <td>Total</td>
+                {total}
+            </tr>
+        )
 
     }
-  });
+});
--- Assignment1/app/assets/javascripts/components/main.js.jsx
+++ Assignment1/app/assets/javascripts/components/main.js.jsx
@@ -1,89 +1,87 @@
 var Main = React.createClass({
 
-  getInitialState: function() {
-    return {
-      'input_group_by': '',
-      'input_sort_by': '',
-      'input_display_arr': '',
-      'year_1': '',
-      'year_2': '',
-      'should_compare': '',
-      'should_total': '',
-      'submitted': false,
-      'students':[]
-    };
-  },
-
-  handleSubmit: function(e) {
-    e.preventDefault();
-    this.fetchData();
-    this.setState({'submitted': true });
-  },
-
-
-  fetchData: function(){
-    $.ajax({
-      url: '/display',
-      dataType: 'json',
-      type: 'POST',
-      data: {
-        input_group_by: this.state.input_group_by,
-        input_sort_by: this.state.input_sort_by,
-        input_display_arr: this.state.input_display_arr
-      },
-      success: function(result) {
-        this.setState({students: result[0],
-          input_display_arr: result[1]});
-          console.log(JSON.stringify(this.state.students));
-          console.log(JSON.stringify(this.state.input_display_arr));
-        }.bind(this),
-        error: function(xhr, status, err) {
-          // this.setState({students: user});
-          console.error(this.props.url, status, err.toString());
-        }.bind(this)
-      });
+    getInitialState: function() {
+        return {
+            'input_group_by': '',
+            'input_sort_by': '',
+            'input_display_arr': '',
+            'year_1': '',
+            'year_2': '',
+            'should_compare': '',
+            'should_total': '',
+            'submitted': false,
+            'students': []
+        };
     },
 
-    handleChange: function (name, e) {
-      var change = {};
-      change[name] = e.target.value;
-      this.setState(change);
+    handleSubmit: function(e) {
+        e.preventDefault();
+        this.fetchData();
+        this.setState({'submitted': true});
+    },
+
+    fetchData: function() {
+        $.ajax({
+            url: '/display',
+            dataType: 'json',
+            type: 'POST',
+            data: {
+                input_group_by: this.state.input_group_by,
+                input_sort_by: this.state.input_sort_by,
+                input_display_arr: this.state.input_display_arr
+            },
+            success: function(result) {
+                this.setState({students: result[0], input_display_arr: result[1]});
+                console.log(JSON.stringify(this.state.students));
+                console.log(JSON.stringify(this.state.input_display_arr));
+            }.bind(this),
+            error: function(xhr, status, err) {
+                // this.setState({students: user});
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    handleChange: function(name, e) {
+        var change = {};
+        change[name] = e.target.value;
+        this.setState(change);
     },
 
     render: function() {
-      if (this.state.submitted) {
-        return <Filter input_display_arr={this.state.input_display_arr}
-          input_group_by={this.state.input_group_by}
-          students={this.state.students}
-          year_1={this.state.year_1}
-          year_2={this.state.year_2}
-          should_total={this.state.should_total}
-          should_compare={this.state.should_compare}/>;
-      }
-      else {
-        return (
-          <div>
-            <form role="form" onSubmit={this.handleSubmit}  >
-              <label ref="input_group_by">Group by: student_id/department/year </label>
-              <input ref="input_group_by" placeholder="" type="text" value={this.state.input_group_by}  onChange={this.handleChange.bind(this, 'input_group_by')}/>
-              <label ref="input_sort_by">SORT BY: maths/physics/chemistry </label>
-              <input ref="input_sort_by" placeholder="" type="text" value={this.state.input_sort_by}  onChange={this.handleChange.bind(this, 'input_sort_by')}/>
-              <label ref="input_display_arr">Display fields: maths,physics,chemistry </label>
-              <input ref="input_display_arr" placeholder="" type="text" value={this.state.input_display_arr}  onChange={this.handleChange.bind(this, 'input_display_arr')}/>
+        if (this.state.submitted) {
+            return <Filter input_display_arr={this.state.input_display_arr} input_group_by={this.state.input_group_by} students={this.state.students} year_1={this.state.year_1} year_2={this.state.year_2} should_total={this.state.should_total} should_compare={this.state.should_compare}/>;
+        } else {
+            return (
+                <div>
+                    <form role="form" onSubmit={this.handleSubmit}>
+                        <label ref="input_group_by">Group by: student_id/department/year
+                        </label>
+                        <input ref="input_group_by" placeholder="" type="text" value={this.state.input_group_by} onChange={this.handleChange.bind(this, 'input_group_by')}/>
+                        <label ref="input_sort_by">SORT BY: maths/physics/chemistry
+                        </label>
+                        <input ref="input_sort_by" placeholder="" type="text" value={this.state.input_sort_by} onChange={this.handleChange.bind(this, 'input_sort_by')}/>
+                        <label ref="input_display_arr">Display fields: maths,physics,chemistry
+                        </label>
+                        <input ref="input_display_arr" placeholder="" type="text" value={this.state.input_display_arr} onChange={this.handleChange.bind(this, 'input_display_arr')}/>
 
-              <label ref="year_1">Year 1 </label>
-              <input ref="year_1" placeholder="" type="text" value={this.state.year_1}  onChange={this.handleChange.bind(this, 'year_1')}/>
-              <label ref="year_2">year_2 </label>
-              <input ref="year_2" placeholder="" type="text" value={this.state.year_2}  onChange={this.handleChange.bind(this, 'year_2')}/>
-              <label ref="should_total">Should total? true/false </label>
-              <input ref="should_total" placeholder="" type="text" value={this.state.should_total}  onChange={this.handleChange.bind(this, 'should_total')}/>
-              <label ref="should_compare">Should compare? true/false </label>
-              <input ref="should_compare" placeholder="" type="text" value={this.state.should_compare}  onChange={this.handleChange.bind(this, 'should_compare')}/>
-              <br/>
-              <input type="submit" value="Submit" />
-            </form>
-          </div>
-        );
-      }
+                        <label ref="year_1">Year 1
+                        </label>
+                        <input ref="year_1" placeholder="" type="text" value={this.state.year_1} onChange={this.handleChange.bind(this, 'year_1')}/>
+                        <label ref="year_2">year_2
+                        </label>
+                        <input ref="year_2" placeholder="" type="text" value={this.state.year_2} onChange={this.handleChange.bind(this, 'year_2')}/>
+                        <label ref="should_total">Should total? true/false
+                        </label>
+                        <input ref="should_total" placeholder="" type="text" value={this.state.should_total} onChange={this.handleChange.bind(this, 'should_total')}/>
+                        <label ref="should_compare">Should compare? true/false
+                        </label>
+                        <input ref="should_compare" placeholder="" type="text" value={this.state.should_compare} onChange={this.handleChange.bind(this, 'should_compare')}/>
+                        <br/>
+                        <input type="submit" value="Submit"/>
+                    </form>
+                </div>
+            );
+        }
     }
-  });
+});
